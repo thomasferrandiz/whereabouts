f5deae0565a669d46519c172289a8b619d959a2a
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2024-08-07 12:42:17 +0000

    fix chart path

--- hack/release/chart-push.sh
+++ hack/release/chart-push.sh
@@ -9,7 +9,7 @@ GITHUB_TOKEN=${GITHUB_TOKEN:-}
 GITHUB_TAG=${GITHUB_TAG:-}
 
 BASE=${PWD}
-HELM_CHART=${BASE}/deployment/whereabouts
+HELM_CHART=${BASE}/deployment/whereabouts-chart
 HELM_CHART_VERSION=${GITHUB_TAG#"v"}
 HELM_CHART_TARBALL="whereabouts-${HELM_CHART_VERSION}.tgz"
 
