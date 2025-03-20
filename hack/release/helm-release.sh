18d9b6c0ecf70d3b540174c2ddaa51d59c299b8d
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2025-03-20 13:39:14 +0000

    move helm release to script

---
+++ hack/release/helm-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+helm package ./deployment/chart/whereabouts --destination chart/ --version ${GITHUB_TAG} --app-version ${GITHUB_TAG}
+# mv chart/whereabouts-chart-$(GITHUB_TAG).tgz chart/whereabouts.tgz
+
+# wget https://${GITHUB_REPO_OWNER}.github.io/whereabouts/index.yaml -O chart/index.yaml || true
+# helm repo index --merge chart/index.yaml --url https://github.com/$(GITHUB_REPO_OWNER)/whereabouts/releases/download/$(GITHUB_TAG)/ chart/
+helm repo index --url https://github.com/${GITHUB_REPO_OWNER}/whereabouts/releases/download/${GITHUB_TAG}/ chart/
+
+
+# TODO move out 
+# gh release upload $(GITHUB_TAG) chart/whereabouts.tgz 
