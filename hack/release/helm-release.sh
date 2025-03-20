befe0f15657423089d0c313e30ad8a08f583ffa8
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2025-03-20 13:52:41 +0000

    upload chart

--- hack/release/helm-release.sh
+++ hack/release/helm-release.sh
@@ -2,7 +2,7 @@
 set -ex
 
 helm package ./deployment/chart/whereabouts --destination chart/ --version ${GITHUB_TAG} --app-version ${GITHUB_TAG}
-# mv chart/whereabouts-chart-$(GITHUB_TAG).tgz chart/whereabouts.tgz
+mv chart/whereabouts-$(GITHUB_TAG).tgz chart/whereabouts.tgz
 
 # wget https://${GITHUB_REPO_OWNER}.github.io/whereabouts/index.yaml -O chart/index.yaml || true
 # helm repo index --merge chart/index.yaml --url https://github.com/$(GITHUB_REPO_OWNER)/whereabouts/releases/download/$(GITHUB_TAG)/ chart/
