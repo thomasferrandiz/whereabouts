4b355977e4ee7031e5a356d3fe4a5a87c81dcc5b
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2025-03-20 13:55:39 +0000

    fix helm relase script

--- hack/release/helm-release.sh
+++ hack/release/helm-release.sh
@@ -2,12 +2,9 @@
 set -ex
 
 helm package ./deployment/chart/whereabouts --destination chart/ --version ${GITHUB_TAG} --app-version ${GITHUB_TAG}
-mv chart/whereabouts-$(GITHUB_TAG).tgz chart/whereabouts.tgz
+mv chart/whereabouts-${GITHUB_TAG}.tgz chart/whereabouts.tgz
 
 # wget https://${GITHUB_REPO_OWNER}.github.io/whereabouts/index.yaml -O chart/index.yaml || true
-# helm repo index --merge chart/index.yaml --url https://github.com/$(GITHUB_REPO_OWNER)/whereabouts/releases/download/$(GITHUB_TAG)/ chart/
+# helm repo index --merge chart/index.yaml --url https://github.com/${GITHUB_REPO_OWNER}/whereabouts/releases/download/${GITHUB_TAG}/ chart/
 helm repo index --url https://github.com/${GITHUB_REPO_OWNER}/whereabouts/releases/download/${GITHUB_TAG}/ chart/
 
-
-# TODO move out 
-# gh release upload $(GITHUB_TAG) chart/whereabouts.tgz 
